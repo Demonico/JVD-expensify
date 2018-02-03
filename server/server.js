9fc7398ff2854bd337893cd719b6b50cafdd6f6d
Jarrod <Demonico@users.noreply.github.com>
2018-02-03 00:05:55 -0700

    typo correction in server.js

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const path = require('path')
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000
-
 const publicPath = path.join(__dirname, '..', 'public');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendfile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
